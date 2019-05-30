fdd3b12457abf2bed979d010767859e61e16ee4c
旭 杨 <27681135@qq.com>
2019-05-31 00:34:26 +0800

    点赞完成

--- routes/apiRoutes/article/like.js
+++ routes/apiRoutes/article/like.js
@@ -7,7 +7,41 @@ var like = function(req, res, next) {
   let params = req.body;
   let params_deco = req.decoded;
 
-  Article.updateOne(
+  Article.findOne({ _id: ObjectID(params._id),"liked.userid": ObjectID(params.userid)},{"liked.$":1}).then(result => {
+    console.log(result);
+    if(result){
+      Article.updateOne({ _id: ObjectID(params._id)},{$pull:{liked:{userid:ObjectID(params.userid)}}},function(err){
+        if(err){
+            res.send(500);
+            console.log(err);
+        }else{
+          return res.send({ status: 1, msg: "取消点赞成功" });
+        }
+    });
+    
+      // result.liked.pull({ userid: ObjectID(params.userid) })
+      // Article.updateOne(
+      //   { _id: ObjectID(params._id) },
+      //   {
+      //     $addToSet: {
+      //       liked: [
+      //         {
+      //           userid: ObjectID(params.userid),
+      //           username: params.username
+      //         }
+      //       ]
+      //     }
+      //   },
+      //   function(err, content) {
+      //     if (err) {
+      //       return res.send({ status: 2, msg: err || "点赞失败" });
+      //     } else {
+      //       return res.send({ status: 1, msg: "点赞成功" });
+      //     }
+      //   }
+      // );
+    }else{
+Article.updateOne(
     { _id: ObjectID(params._id) },
     {
       $addToSet: {
@@ -27,6 +61,14 @@ var like = function(req, res, next) {
       }
     }
   );
+    }
+    // res.send(200,result);
+    // res.status(200).send({ status: 1, msg: "获取成功", data: result });
+  });
+
+
+
+  
 };
 
 module.exports = like;
