9137befb80f83a540e93a5e4be0ba73788e80015
旭 杨 <27681135@qq.com>
2019-06-01 00:43:33 +0800

    关注功能

--- routes/apiRoutes/article/like.js
+++ routes/apiRoutes/article/like.js
@@ -2,65 +2,65 @@
 // var router = express.Router();
 var Article = require("../../../models/article");
 var ObjectID = require("mongodb").ObjectID;
-var like = function(req, res, next) {
+var like = function (req, res, next) {
   // res.send('respond with a resource');
   let params = req.body;
   let params_deco = req.decoded;
 
-  Article.findOne({ _id: ObjectID(params._id),"liked.userid": ObjectID(params.userid)},{"liked.$":1}).then(result => {
+  Article.findOne({
+    _id: ObjectID(params._id),
+    "liked.userid": ObjectID(params.userid)
+  }, {
+    "liked.$": 1
+  }).then(result => {
     console.log(result);
-    if(result){
-      Article.updateOne({ _id: ObjectID(params._id)},{$pull:{liked:{userid:ObjectID(params.userid)}}},function(err){
-        if(err){
-            res.send(500);
-            console.log(err);
-        }else{
-          return res.send({ status: 1, msg: "取消点赞成功" });
-        }
-    });
-    
-      // result.liked.pull({ userid: ObjectID(params.userid) })
-      // Article.updateOne(
-      //   { _id: ObjectID(params._id) },
-      //   {
-      //     $addToSet: {
-      //       liked: [
-      //         {
-      //           userid: ObjectID(params.userid),
-      //           username: params.username
-      //         }
-      //       ]
-      //     }
-      //   },
-      //   function(err, content) {
-      //     if (err) {
-      //       return res.send({ status: 2, msg: err || "点赞失败" });
-      //     } else {
-      //       return res.send({ status: 1, msg: "点赞成功" });
-      //     }
-      //   }
-      // );
-    }else{
-Article.updateOne(
-    { _id: ObjectID(params._id) },
-    {
-      $addToSet: {
-        liked: [
-          {
-            userid: ObjectID(params.userid),
-            username: params.username
+    if (result) {
+      Article.updateOne({
+        _id: ObjectID(params._id)
+      }, {
+        $pull: {
+          liked: {
+            userid: ObjectID(params.userid)
           }
-        ]
-      }
-    },
-    function(err, content) {
-      if (err) {
-        return res.send({ status: 2, msg: err || "点赞失败" });
-      } else {
-        return res.send({ status: 1, msg: "点赞成功" });
-      }
-    }
-  );
+        }
+      }, function (err) {
+        if (err) {
+          res.send(500);
+          console.log(err);
+        } else {
+          return res.send({
+            status: 1,
+            msg: "取消点赞成功"
+          });
+        }
+      });
+
+      
+    } else {
+      Article.updateOne({
+          _id: ObjectID(params._id)
+        }, {
+          $addToSet: {
+            liked: [{
+              userid: ObjectID(params.userid),
+              username: params.username
+            }]
+          }
+        },
+        function (err, content) {
+          if (err) {
+            return res.send({
+              status: 2,
+              msg: err || "点赞失败"
+            });
+          } else {
+            return res.send({
+              status: 1,
+              msg: "点赞成功"
+            });
+          }
+        }
+      );
     }
     // res.send(200,result);
     // res.status(200).send({ status: 1, msg: "获取成功", data: result });
@@ -68,7 +68,7 @@ Article.updateOne(
 
 
 
-  
+
 };
 
 module.exports = like;
--- routes/apiRoutes/user/follow.js
+++ routes/apiRoutes/user/follow.js
@@ -2,31 +2,125 @@
 // var router = express.Router();
 var User = require("../../../models/user");
 var ObjectID = require("mongodb").ObjectID;
-var follow = function(req, res, next) {
+var follow = function (req, res, next) {
   // res.send('respond with a resource');
   let params = req.body;
   let params_deco = req.decoded;
 
-  User.updateOne(
-    { _id: ObjectID(params._id) },
-    {
-      $addToSet: {
-        following: [
-          {
-            userid: ObjectID(params.userid),
-            username: params.username
+  User.findOne({
+    _id: ObjectID(params.userid),
+    "following.userid": ObjectID(params._id)
+  }, {
+    "following.$": 1
+  }).then(result => {
+    console.log(result);
+    if (result) {
+      User.updateOne({
+        _id: ObjectID(params.userid)
+      }, {
+        $pull: {
+          following: {
+            userid: ObjectID(params._id)
           }
-        ]
-      }
-    },
-    function(err, content) {
-      if (err) {
-        return res.send({ status: 2, msg: err || "点赞失败" });
-      } else {
-        return res.send({ status: 1, msg: "点赞成功" });
-      }
+        }
+      }, function (err) {
+        if (err) {
+          res.send({
+            status: 2,
+            msg: err || "取关失败"
+          });
+          console.log(err);
+        } else {
+          User.findOne({
+            _id: ObjectID(params._id),
+            "followed.userid": ObjectID(params.userid)
+          }, {
+            "followed.$": 1
+          }).then(result => {
+            console.log(result);
+            if (result) {
+              User.updateOne({
+                _id: ObjectID(params._id)
+              }, {
+                $pull: {
+                  followed: {
+                    userid: ObjectID(params.userid)
+                  }
+                }
+              }, function (err) {
+                if (err) {
+                  return res.send({
+                    status: 2,
+                    msg: err || "取关失败"
+                  });
+                } else {
+                  return res.send({
+                    status: 1,
+                    msg: "取关成功"
+                  });
+                }
+              })
+            }
+          })
+
+        }
+      });
+
+
+    } else {
+      //_id为发起关注者，userid name 为发起关注请求者
+      User.updateOne({
+          _id: ObjectID(params._id)
+        }, {
+          $addToSet: {
+            followed: [{
+              userid: ObjectID(params.userid),
+              username: params.username
+            }]
+          }
+        },
+        function (err, content) {
+          if (err) {
+            return res.send({
+              status: 2,
+              msg: err || "关注失败"
+            });
+          } else {
+            User.updateOne({
+                _id: ObjectID(params.userid)
+              }, {
+                $addToSet: {
+                  following: [{
+                    userid: ObjectID(params._id),
+                    username: params.aimusername
+                  }]
+                }
+              },
+              function (err, content) {
+                if (err) {
+                  return res.send({
+                    status: 2,
+                    msg: err || "关注失败"
+                  });
+                } else {
+                  return res.send({
+                    status: 1,
+                    msg: "关注成功"
+                  });
+                }
+              }
+            );
+            // return res.send({ status: 1, msg: "点赞成功" });
+          }
+        }
+      );
     }
-  );
+  })
+
+
+
+
+
 };
 
 module.exports = follow;
