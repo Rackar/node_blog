1ae75c5bdaa16973b4f97ecb7844d4c059e8636d
Your Name <27681135@qq.com>
2019-06-11 11:01:57 +0800

    编辑文章时可加入图片

--- routes/apiRoutes/article/edit.js
+++ routes/apiRoutes/article/edit.js
@@ -6,31 +6,29 @@ var edit = function(req, res, next) {
   // res.send('respond with a resource');
   let params = req.body;
   let params_deco = req.decoded;
-  if(params_deco.userid!=params.userid)
-  {res.send({ status: 0, msg: "请重新登陆" })
-
-}else{
-  Article.updateOne(
-    { _id: ObjectID(params._id) },
-    {
-      $set: {
-        title: params.title,
-        content: params.content,
-        output: params.output,
-        publicdate:new Date(params.time)
+  if (params_deco.userid != params.userid) {
+    res.send({ status: 0, msg: "请重新登陆" });
+  } else {
+    Article.updateOne(
+      { _id: ObjectID(params._id) },
+      {
+        $set: {
+          title: params.title,
+          content: params.content,
+          output: params.output,
+          publicdate: new Date(params.time),
+          previewImageId: params.previewImageId
+        }
+      },
+      function(err, content) {
+        if (err) {
+          return res.send({ status: 2, msg: err || "文章编辑失败" });
+        } else {
+          return res.send({ status: 1, msg: "文章编辑成功" });
+        }
       }
-    },
-    function(err, content) {
-      if (err) {
-        return res.send({ status: 2, msg: err || "文章编辑失败" });
-      } else {
-        return res.send({ status: 1, msg: "文章编辑成功" });
-      }
-    }
-  );
-}
-
-  
+    );
+  }
 };
 
 module.exports = edit;
